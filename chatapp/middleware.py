a8b11bf39024e2fda485d3bdb8bb11368a94427d
Md. Asadozzaman <83003038+asadozzaman@users.noreply.github.com>
2024-11-29 14:56:25 +0600

    change midd dfg

--- chatapp/middleware.py
+++ chatapp/middleware.py
@@ -1,16 +1,14 @@
-import logging
-logger = logging.getLogger(__name__)
-
 class SetSessionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # Get 'session_uid' from cookies
         session_uid = request.COOKIES.get('session_uid')
+
         if session_uid:
-            logger.debug(f"Session UID found: {session_uid}")
+            # Store it in the Django session
             request.session['uid'] = session_uid
-        else:
-            logger.debug("No Session UID found in cookies.")
+
         response = self.get_response(request)
         return response
